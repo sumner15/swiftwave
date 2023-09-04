ad4d43b088fa5784e76cdbd0e8c8275fd7b41d47
Sumner L Norman <sumner.norman@gmail.com>
2023-09-04 12:00:53 -0700

    initial commit swiftwave template only

---
+++ swiftwave/swiftwave/swiftwaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftwaveApp.swift
+//  swiftwave
+//
+//  Created by Sumner Norman on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftwaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
